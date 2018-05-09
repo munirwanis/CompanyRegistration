c050dbbbbc5de9991c420327ca55008c5462e11c
Munir Wanis <fallen.angel.pp@gmail.com>
2018-05-09 18:12:57 -0300

    ➕ Adds ViewControllers to missing layouts

---
+++ CompanyRegistration/Presentation/CreateCompany/CreateCompanyTableViewController.swift
@@ -0,0 +1,25 @@
+//
+//  CreateCompanyTableViewController.swift
+//  CompanyRegistration
+//
+//  Created by Munir Wanis on 09/05/18.
+//  Copyright © 2018 Wanis Corporation. All rights reserved.
+//
+
+import UIKit
+
+class CreateCompanyTableViewController: UITableViewController {
+    @IBOutlet weak var ownerNameTextField: UITextField!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var phoneTextField: UITextField!
+    @IBOutlet weak var companyTextField: UITextField!
+    @IBOutlet weak var cnpjTextField: UITextField!
+    @IBOutlet weak var activationDateTextField: UITextField!
+    @IBOutlet weak var isMeiSwitch: UISwitch!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+}
---
+++ CompanyRegistration/Presentation/DetailCompany/DetailCompanyTableViewController.swift
@@ -0,0 +1,25 @@
+//
+//  DetailCompanyTableViewController.swift
+//  CompanyRegistration
+//
+//  Created by Munir Wanis on 09/05/18.
+//  Copyright © 2018 Wanis Corporation. All rights reserved.
+//
+
+import UIKit
+
+class DetailCompanyTableViewController: UITableViewController {
+    @IBOutlet weak var companyNameLabel: UILabel!
+    @IBOutlet weak var ownerNameLabel: UILabel!
+    @IBOutlet weak var emailLabel: UILabel!
+    @IBOutlet weak var phoneLabel: UILabel!
+    @IBOutlet weak var cnpjLabel: UILabel!
+    @IBOutlet weak var activitiesDateLabel: UILabel!
+    @IBOutlet weak var isMeiLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+}
